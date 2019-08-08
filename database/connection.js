be5404c735c8c1864d9e995734b8004f34f41613
Kevin Park <kevypark@Kevins-MacBook-Pro.local>
2019-08-08 18:22:17 -0400

    changed hostname

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  host: "ec2-54-165-188-232.compute-1.amazonaws.com",
+  host: "ec2-3-86-58-118.compute-1.amazonaws.com",
   database: "greenfield",
   password: "",
   port: 5432
