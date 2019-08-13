1354834011885f45bfeb934008e5fe6b486321d3
Kevin Park <kevypark@Kevins-MacBook-Pro.local>
2019-08-13 11:53:11 -0400

    load balancer

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  host: "172.31.93.157",
+  host: "http://greenfield-364062280.us-east-1.elb.amazonaws.com",
   database: "greenfield",
   password: "",
   port: 5432
