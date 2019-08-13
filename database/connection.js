d97bb1f70680c4b584fa8de33b406deddfe8891f
Kevin Park <kevypark@Kevins-MacBook-Pro.local>
2019-08-13 15:31:15 -0400

    test

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
+  user: "kevypark",
+  host: "ec2-52-90-110-238.compute-1.amazonaws.com",
   database: "greenfield",
-  password: "",
+  password: "password",
   port: 5432
 });
 
